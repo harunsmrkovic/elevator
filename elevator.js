1a8189307bc69f6c718e8fd4e3daa6c58a427815
Harun Smrkovic <harun@personal.com>
2014-12-13 15:54:53 +0100

    more work

--- elevator.js
+++ elevator.js
@@ -10,6 +10,14 @@
 
 	var elevatorSpeed = 1000; // miliseconds per floor
 	var engines = [];
+
+	var waitOnFloorTime = 3000;
+
+	var queue = [];
+
+	function sortNumber(a,b) {
+    return a - b;
+	}
 	
 	Building.buildBuilding = function(){
 		for(var ei = 0; ei < numOfElevators; ei++){
@@ -25,6 +33,7 @@
 				$('#building .elevator[data-elevator="'+ei+'"').append('<div class="floor" data-floor="'+fi+'"><i class="fa fa-chevron-up call-up"></i> <span>'+fi+'</span> <i class="fa fa-chevron-down call-down"></i></div>');
 			}
 			elevators.push(elevator);
+			queue.push([]);
 		}
 		$('#building').append('<div id="basement"></div>');
 	}
@@ -37,31 +46,76 @@
 		}
 	}
 
-	// this function will add floor that elevator has to go to to queue
-	Building.callElevator = function(elevator, targetFloor){
+	// main AI
+	Building.doMagic = function(){
+		for(var elevator = 0; elevator < queue.length; elevator++){
+			console.log('magicing elevator', elevator);
+			if(queue[elevator].length !== 0){
+				queue[elevator].sort(sortNumber);
+				console.log(queue[elevator]);
+				Building.sendToFloor(elevator, queue[elevator][0]).then(function(nowOnFloor){
+					queue[elevator].shift();
+					if(queue[elevator].length !== 0){
+						setTimeout(function(){
+							console.log('waitah')
+							Building.doMagic();
+						}, waitOnFloorTime);
+					}
+					// console.warn('elevator arrived!');
+					// console.log('wait for', waitOnFloorTime);
+					// (function(elevator){
+					// 	console.log('initing timeout on elevator', elevator);
+					// 	setTimeout(function(){
+					// 		console.log('elevator', elevator);
+					// 		console.log('queue', queue);
+					// 		queue[elevator].shift();
+					// 		if(queue[elevator].length !== 0){
+					// 			Building.doMagic();
+					// 		}
+					// 	}, waitOnFloorTime);
+					// })(elevator);
+				});
+			}
+		}
+
+	}
+
+	// call an elevator (to go up or down) from a certain floor
+	Building.callElevator = function(calledFromFloor){
 		// console.log(elevators);
 		// elevator = (typeof elevator === 'object') ? elevator : elevators[elevator];
 		// elevator[elevator.indexOf(1)] = 0;
 		// elevator[targetFloor] = 1;
-		Building.sendToFloor(elevator, targetFloor);
+		var elevator = 1;
+		// Building.sendToFloor(elevator, calledFromFloor);
 	}
 
+	// require to go to a certain floor from inside an elevator
+	Building.requireOnFloor = function(elevator, targetFloor){
+		queue[elevator].push(targetFloor);
+		Building.doMagic();
+		// Building.sendToFloor(elevator, targetFloor);
+	}
+
+	// this fires up the engine and moves the elevator to desired floor
 	Building.sendToFloor = function(elevator, targetFloor){
-		clearInterval(engines[elevator]);
-		engines[elevator] = setInterval(function(){
-			if(Building.getCurrentFloor(elevator) !== targetFloor){
-				if(Building.getCurrentFloor(elevator) - targetFloor <= 0){
-					Building.moveFloorUp(elevator);
+		return new Promise(function(resolve, reject){
+			clearInterval(engines[elevator]);
+			engines[elevator] = setInterval(function(){
+				if(Building.getCurrentFloor(elevator) !== targetFloor){
+					if(Building.getCurrentFloor(elevator) - targetFloor <= 0){
+						Building.moveFloorUp(elevator);
+					}
+					else {
+						Building.moveFloorDown(elevator);
+					}
 				}
 				else {
-					Building.moveFloorDown(elevator);
+					clearInterval(engines[elevator]);
+					resolve(targetFloor);
 				}
-			}
-			else {
-				// $('#building .elevator[data-elevator="'+elevator+'"] .floor[data-floor="'+targetFloor+'"]').removeClass('calledElevator');
-				clearInterval(engines[elevator]);
-			}
-		}, elevatorSpeed);
+			}, elevatorSpeed);
+		});
 	}
 
 	Building.moveFloorDown = function(elevator){
@@ -97,19 +151,13 @@ $(document).ready(function(){
 	$('#building').on('click', '.floor span', function(){
 		var floor = $(this).parent();
 		floor.addClass('calledFromElevator');
-		Building.callElevator(parseInt(floor.parent().attr('data-elevator')), parseInt(floor.attr('data-floor')));
+		Building.requireOnFloor(parseInt(floor.parent().attr('data-elevator')), parseInt(floor.attr('data-floor')));
 	});
 
-	$('#building').on('click', '.floor .call-up', function(){
-		var floor = $(this).parent();
-		floor.addClass('calledUp');
-		Building.callElevator(parseInt(floor.parent().attr('data-elevator')), parseInt(floor.attr('data-floor')));
-	});
-
-	$('#building').on('click', '.floor .call-down', function(){
-		var floor = $(this).parent();
-		floor.addClass('calledDown');
-		Building.callElevator(parseInt(floor.parent().attr('data-elevator')), parseInt(floor.attr('data-floor')));
+	$('#building').on('click', '.floor .call-up, .floor .call-down', function(){
+		var floor = $(this).parent().attr('data-floor');
+		$('.floor[data-floor="'+floor+'"]').addClass(($(this).hasClass('call-up')) ? 'calledUp' : 'calledDown');
+		Building.callElevator(parseInt(floor));
 	});
 
 });
